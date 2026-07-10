d4c8d1921a847e670456cf967ad2312a2135c06e
Nguyen Dinh Tuan Dat <legendboy.vn@gmail.com>
2026-07-10 12:07:54 +0700

    add database migration v1(create tenant), v2(create íntance)

---
+++ src/main/resources/db.migration/V1__create_tenants.sql
@@ -0,0 +1,35 @@
+CREATE TABLE tenants (
+    id                          UUID PRIMARY KEY,
+    username                    TEXT UNIQUE NOT NULL,
+
+    quota_cpu                   INT NOT NULL DEFAULT 10,
+    quota_ram_mb                INT NOT NULL DEFAULT 8192,
+    quota_instance_count        INT NOT NULL DEFAULT 5,
+
+    allocated_cpu               INT NOT NULL DEFAULT 0,
+    allocated_ram_mb            INT NOT NULL DEFAULT 0,
+    allocated_instance_count    INT NOT NULL  DEFAULT 0,
+
+    version                     BIGINT NOT NULL DEFAULT 0,
+
+    created_at                  TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at                  TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    CONSTRAINT chk_tenant_quota_cpu_positive
+        CHECK ( quota_cpu > 0 ),
+
+    CONSTRAINT chk_tenant_quota_ram_mb_positive
+        CHECK ( quota_ram_mb > 0 ),
+
+    CONSTRAINT chk_tenant_quota_instance_positive
+        CHECK ( quota_instance_count > 0 ),
+
+    CONSTRAINT chk_tenant_allocated_cpu_valid
+        CHECK ( allocated_cpu >= 0 AND allocated_cpu <= quota_cpu ),
+
+    CONSTRAINT chk_tenant_allocated_ram_valid
+        CHECK (allocated_ram_mb >= 0 AND allocated_ram_mb <= quota_ram_mb),
+
+    CONSTRAINT chk_tenant_allocated_instance_valid
+        CHECK (allocated_instance_count >= 0 AND allocated_instance_count <= quota_instance_count)
+)
---
+++ src/main/resources/db.migration/V2__create_instances.sql
@@ -0,0 +1,77 @@
+CREATE TABLE instances (
+        id              UUID PRIMARY KEY,
+
+        tenant_id       UUID NOT NULL REFERENCES tenants(id),
+
+        name            TEXT NOT NULL,
+        image_name      TEXT NOT NULL,
+
+        cpu_allocated   INT NOT NULL,
+        ram_mb          INT NOT NULL,
+
+        desired_state   TEXT NOT NULL,
+        current_state   TEXT NOT NULL,
+
+        container_id    TEXT,
+
+        failure_reason  TEXT,
+
+        version         BIGINT NOT NULL DEFAULT 0,
+
+        created_at      TIMESTAMPTZ NOT NULL DEFAULT now(),
+        updated_at      TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+        CONSTRAINT uq_instances_tenant_name
+            UNIQUE (tenant_id, name),
+
+        CONSTRAINT chk_instance_cpu_positive
+                               CHECK (cpu_allocated > 0),
+
+        CONSTRAINT chk_instance_ram_positive
+                               CHECK (ram_mb > 0),
+
+        CONSTRAINT chk_instance_desired_state
+                               CHECK (desired_state IN (
+                                                        'RUNNING',
+                                                        'STOPPED',
+                                                        'DELETED'
+                                   )),
+
+        CONSTRAINT chk_instance_current_state
+                               CHECK (current_state IN (
+                                                        'PENDING',
+                                                        'PROVISIONING',
+                                                        'STARTING',
+                                                        'RUNNING',
+                                                        'STOPPING',
+                                                        'STOPPED',
+                                                        'DELETING',
+                                                        'DELETED',
+                                                        'FAILED'
+                                   )),
+
+        CONSTRAINT chk_instance_container_id_state
+                               CHECK (
+                                   container_id IS NULL
+                                       OR current_state IN (
+                                                            'RUNNING',
+                                                            'STOPPING',
+                                                            'STOPPED',
+                                                            'DELETING',
+                                                            'FAILED'
+                                       )
+                                   )
+);
+
+    CREATE INDEX idx_instances_tenant
+        ON instances(tenant_id);
+
+    CREATE INDEX idx_instances_current_state_updated_at
+        ON instances(current_state, updated_at);
+
+    CREATE INDEX idx_instances_desired_current
+        ON instances(desired_state, current_state);
+
+    CREATE INDEX idx_instances_drift
+        ON instances(desired_state, current_state, updated_at)
+        WHERE desired_state != current_state;
