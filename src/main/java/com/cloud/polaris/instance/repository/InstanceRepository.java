5ad571cf8f758393b83bab2356bfa3235088302e
Nguyen Dinh Tuan Dat <legendboy.vn@gmail.com>
2026-07-10 17:55:14 +0700

    Add JPA repositories for tenants and instances

--- src/main/java/com/cloud/polaris/instance/repository/InstanceRepository.java
+++ src/main/java/com/cloud/polaris/instance/repository/InstanceRepository.java
@@ -1,4 +1,13 @@
 package com.cloud.polaris.instance.repository;
 
-public class InstanceRepository {
+import com.cloud.polaris.instance.domain.Instance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface InstanceRepository extends JpaRepository<Instance, UUID> {
+    boolean existsByTenantIdAndName(UUID tenantId, String name);
+    Optional<Instance> findByIdAndTenantId(UUID id, UUID tenantId);
 }
+
--- src/main/java/com/cloud/polaris/tenant/repository/TenantRepository.java
+++ src/main/java/com/cloud/polaris/tenant/repository/TenantRepository.java
@@ -1,4 +1,21 @@
 package com.cloud.polaris.tenant.repository;
 
-public interface TenantRepository {
+import com.cloud.polaris.instance.domain.Instance;
+import com.cloud.polaris.tenant.domain.Tenant;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface  TenantRepository extends JpaRepository<Tenant, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM Tenant t WHERE t.id = :id")
+    Optional<Tenant> findByIdForUpdate(@Param("id") UUID id);
+
+    boolean existsByUsername(String username);
 }
